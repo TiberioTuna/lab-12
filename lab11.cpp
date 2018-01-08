da68a5fda51752f569e43941b97ddd89abe92fce
Niklai80 <tirla.nicolae80@gmail.com>
2018-01-08 08:50:50 +0200

    Add files via upload

---
+++ lab11.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+template <typename T>
+class Stack
+{
+public:
+    bool empty() const
+    {
+        return (a.empty());
+    }
+    void push(const T& ob)
+    {
+        a.push_back(ob);
+    }
+    const T& top() const
+    {
+        if (a.empty())
+            printf("Stiva goala\n");
+        return a.back();
+    }
+    void pop()
+    {
+        if (a.empty())
+            cout<<"Stiva goala\n";
+        a.pop_back();
+    }
+
+    vector <T> a;
+};
+
+int main()
+{
+    Stack <int> b_int;
+    int a[10];
+    for (int i = 1; i <= 4; i++)
+    {
+        printf("Elementul %d: ", i);
+        cin>>a[i];
+        b_int.push(a[i]);
+    }
+    while (!b_int.empty())
+    {
+        cout << b_int.top() << "\n";
+        b_int.pop();
+    }
+    cout << endl;
+
+    Stack <char> b_char;
+    char c[10];
+    for (int i = 1; i <= 4; i++)
+    {
+        printf("Litera %d: ", i);
+        cin>>c[i];
+        b_char.push(c[i]);
+    }
+    while (!b_char.empty())
+    {
+        cout << b_char.top() << "\n";
+        b_char.pop();
+    }
+
+    return 0;
+}
